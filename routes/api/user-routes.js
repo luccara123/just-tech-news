9dd7da0396e8c9280f0d8833af71a76628b92e82
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-10 18:09:12 -0700

    added crud

---
+++ routes/api/user-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    }) // same as SELCT * FROM users*
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({                       // same as INSERT INTO users
+                                        // (username, email, password)
+                                        // VALUES
+                                       //("Lernantino", "lernantino@gmail.com", "password1234");
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+// same as 
+//UPDATE users
+//SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
+//WHERE id = 1;
+// DELETE /api/users/1
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
