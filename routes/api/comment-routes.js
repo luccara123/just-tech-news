2217bfda04d97bdbf5e2af4711b0775d5bc752e6
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-14 15:59:47 -0700

    added comments feature

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// Get comments
+router.get('/', (req, res) => {
+    // Access the Comment model and run .findAll() method to get all comments
+    Comment.findAll()
+      // return the data as JSON formatted
+      .then(dbCommentData => res.json(dbCommentData))
+      // if there is a server error, return that error
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+});
+
+// Delete a comment
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCommentData => {
+          if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+          }
+          res.json(dbCommentData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+
+module.exports = router;
