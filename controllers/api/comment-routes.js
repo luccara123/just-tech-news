1526b4782a99422ec549fa8407e828052d20f051
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-23 21:38:03 -0700

    added homepage view

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -28,23 +28,23 @@ router.post('/', (req, res) => {
 });
 
 // Delete a comment
-router.delete('/:id', withAuth, (req, res) => {
-    Comment.destroy({
-        where: {
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+      where: {
           id: req.params.id
-        }
-      })
-        .then(dbCommentData => {
-          if (!dbCommentData) {
-            res.status(404).json({ message: 'No comment found with this id' });
-            return;
-          }
-          res.json(dbCommentData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
-    });
+      }
+  })
+  .then(dbCommentData => {
+      if(!dbCommentData){
+          res.status(400).json({message: 'No comment found with this id'});
+          return;
+      }
+      res.json(dbCommentData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
 
 module.exports = router;
